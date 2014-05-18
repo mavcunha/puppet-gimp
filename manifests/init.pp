9471abd5f1bf410a2ea0520827fbaf12c4ca405b
Marco Valtas <mavcunha@gmail.com>
2014-05-18 20:43:36 -0300

    Updated to Gimp 2.8.10, support to build on snap

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class gimp {
   package { 'GNU Image Manipulation Program':
     provider => 'appdmg',
-    source   => 'ftp://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.4-nopython-dmg-1.dmg',
+    source   => 'ftp://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.10-dmg-1.dmg',
   }
 }
--- spec/classes/gimp_spec.rb
+++ spec/classes/gimp_spec.rb
@@ -4,7 +4,7 @@ describe 'gimp' do
   it do
     should contain_package('GNU Image Manipulation Program').with({
       :provider => 'appdmg',
-      :source   => 'ftp://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.4-nopython-dmg-1.dmg',
+      :source   => 'ftp://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.10-dmg-1.dmg',
     })
   end
 end
